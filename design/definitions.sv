3161b1e262d2f0fdffd3dd88f30a7b3fc50f638a
deepthipsu <chevuru@pdx.edu>
2024-02-03 13:50:12 -0800

    fifomem

--- design/definitions.sv
+++ design/definitions.sv
@@ -1,6 +1,7 @@
 package definitions;
 
-parameter [3:0] ADDRSIZE = 4'd9;
+parameter [3:0] ADDRSIZE = 4'd10;
+parameter [3:0] DATASIZE = 4'd8;
 logic [1:0] wr_clk = 2;
 
 
---
+++ design/fifomem.sv
@@ -0,0 +1,32 @@
+
+
+import definitions::*;
+
+`timescale 1ns/100ps;
+
+// FIFO memory buffer
+module fifomem 
+( output logic [DATASIZE-1:0] rdata,
+  input  logic [DATASIZE-1:0] wdata,
+  input  logic [ADDRSIZE-1:0] wptr, rptr,
+  input  logic wack, wclk, wfull,
+  input  logic rack, rclk, remty,
+  output  logic rinc, winc);
+
+
+  localparam DEPTH = 1<<ADDRSIZE;
+  logic [DATASIZE-1:0] mem [0:DEPTH-1];
+
+  always@(posedge rclk)
+begin
+  rdata <= (rack & ~remty) ? mem[rptr] : rdata;
+  rinc <= (rack & ~remty) ? rinc+1 : rinc;
+end
+
+  always @(posedge wclk)
+begin
+  mem[wptr] <= (wack & ~wfull) ? wdata : mem[wptr];
+  winc <= (wack & ~wfull) ? winc+1 : winc;
+end
+
+endmodule
