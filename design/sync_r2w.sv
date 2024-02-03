4f16121c95da6b567d62483fdd2e7f3337561623
deepthipsu <chevuru@pdx.edu>
2024-02-02 16:19:53 -0800

    updated files

--- design/sync_r2w.sv
+++ design/sync_r2w.sv
@@ -7,11 +7,19 @@ import definitions::*;
 // Read-domain to write-domain synchronizer
 module sync_r2w 
 ( output logic [ADDRSIZE:0] wq1_rptr,
+  output logic inc_o,
   input  logic [ADDRSIZE:0] rptr,
-  input  logic wclk, wrst_n);
-
+  input  logic inc, wclk, wrst_n);
+ 
+  logic [ADDRSIZE:0]rptr_i;
 
   always_ff @(posedge wclk or negedge wrst_n)
     if (!wrst_n) wq1_rptr <= 0;
-    else wq1_rptr <= rptr + 1;
+    else begin
+	  wq1_rptr <= rptr_i;
+	  inc_o <= 0;
+    end
+
+  assign rptr_i = inc ? rptr + 1 : rptr;
+
 endmodule
--- design/sync_w2r.sv
+++ design/sync_w2r.sv
@@ -6,10 +6,20 @@ import definitions::*;
 // Write-domain to read-domain synchronizer
 module sync_w2r 
 ( output logic [ADDRSIZE:0] rq1_wptr,
+  output logic inc_o,
   input  logic [ADDRSIZE:0] wptr,
-  input  logic rclk, rrst_n);
+  input  logic inc, rclk, rrst_n);
+
+ logic [ADDRSIZE:0]wptr_i;
 
   always_ff @(posedge rclk or negedge rrst_n)
     if (!rrst_n) rq1_wptr <= 0;
-    else rq1_wptr <= wptr + 1;
+    else begin
+	rq1_wptr <= wptr_i;
+	inc_o <= 0;
+    end
+
+assign wptr_i = inc ? wptr + 1 : wptr;
+
 endmodule
+
