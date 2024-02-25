0e61c44347491d56f65c6af6e7364e2d05b64672
masaaki10 <72476061+masaaki10@users.noreply.github.com>
2024-02-25 15:21:40 -0800

    Update definitions.sv

--- design/uvm_based_testbench/definitions.sv
+++ design/uvm_based_testbench/definitions.sv
@@ -12,11 +12,14 @@ package definitions;
 //import uvm_pkg::*;
 parameter ADDRSIZE = 10;
 parameter DATASIZE = 9;
-parameter POINTERSIZE = 9;
+parameter POINTERSIZE = 10;
 parameter DEPTH = 512;
 parameter WRITE_PERIOD = 2; // Number of clock cycles between successive writes
 parameter READ_PERIOD = 1; // Number of clock cycles between successive reads
 parameter BURST_LENGTH = 1024; // Burst length
+    logic [1:0] write_count = 0; // write idle cyle is 2
+    logic [1:0] read_count = 0; // read idle cycle is 1
+    logic [10:0] burst_count = 0; // 2^10 = 1024 
 //`include "transaction.sv"
 //`include "generator.sv"
 //`include "driver.sv"
