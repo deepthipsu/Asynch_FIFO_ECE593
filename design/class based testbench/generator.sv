c31574f08bad78978b7e34d578b911487761cafc
masaaki10 <72476061+masaaki10@users.noreply.github.com>
2024-03-03 18:31:20 -0800

    Update generator.sv

--- design/class based testbench/generator.sv
+++ design/class based testbench/generator.sv
@@ -12,12 +12,12 @@ import definitions::*;
 
 class generator;
 rand transaction trans;
+int repeat_count;
 
 mailbox gen2driv;
 
 event ended;
 
-int repeat_count;
 
 function new(mailbox gen2driv);
   this.gen2driv = gen2driv;
@@ -25,9 +25,10 @@ endfunction
 
 task main();
 $display("Generator started");
-  repeat(repeat_count) begin
+  for(int i = 1; i<=repeat_count; i++) begin 
   trans= new();
-  if(!trans.randomize()) $fatal("Gen:: trans randomization failed");
+if (i <= repeat_count) assert(trans.randomize() with {trans.transactionID == i;}) 
+	else $fatal("Gen:: trans randomization failed");
       trans.display("[ Generator ]");
       gen2driv.put(trans);
   end
